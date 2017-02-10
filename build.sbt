121f66b01140df6dc322c2f4f037a439fcf405f9
Mark Sage <msage@rewardsnetwork.com>
2017-02-09 19:05:43 -0600

    initial commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "scala-seed"
+
+version := "1.0"
+
+scalaVersion := "2.11.1"
+
+
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "3.0.1"
+
+    
---
+++ src/test/scala-2.11/StackSpec.scala
@@ -0,0 +1,46 @@
+import org.scalatest._
+
+import scala.collection.mutable.{ListBuffer, Stack}
+import org.scalatest.Assertions
+
+class StackSpec extends FlatSpec {
+
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    assert(stack.pop() === 2)
+    assert(stack.pop() === 1)
+  }
+
+  it should "throw NoSuchElementException if an empty stack is popped" in {
+    val emptyStack = new Stack[String]
+    assertThrows[NoSuchElementException] {
+      emptyStack.pop()
+    }
+  }
+  "Two arrays" should "be equal if all elements are the same" in {
+    assert(Array(1,2,4,5) === Array(1,2,4,5))
+  }
+  def fixture =
+    new {
+      val builder = new StringBuilder("ScalaTest is ")
+      val buffer = new ListBuffer[String]
+    }
+
+  "Testing" should "be easy" in {
+    val f = fixture
+    f.builder.append("easy!")
+    assert(f.builder.toString === "ScalaTest is easy!")
+    assert(f.buffer.isEmpty)
+    f.buffer += "sweet"
+  }
+
+  it should "be fun" in {
+    val f = fixture
+    f.builder.append("fun!")
+    assert(f.builder.toString === "ScalaTest is fun!")
+    assert(f.buffer.isEmpty)
+  }
+
+}
